57464c9c98332ff110da8d7fa6375e5a4fda0e55
Christopher Pomposo <chris2017pg@gmail.com>
2025-02-27 23:26:02 -0600

    installed firebase

--- src/composables/useFirebase.js
+++ src/composables/useFirebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyASyVqMwtRcxjr7_r8Fd5y2AGMIrzaBqlA",
-  authDomain: "company-directory-d9267.firebaseapp.com",
-  projectId: "company-directory-d9267",
-  storageBucket: "company-directory-d9267.firebasestorage.app",
-  messagingSenderId: "570439959580",
-  appId: "1:570439959580:web:a878ac663ba653f1670bd9"
+  apiKey: "AIzaSyBo6HbA27EhygstsHNPoV96Z7dAoXXJnkE",
+  authDomain: "project5-directory.firebaseapp.com",
+  projectId: "project5-directory",
+  storageBucket: "project5-directory.firebasestorage.app",
+  messagingSenderId: "841690344791",
+  appId: "1:841690344791:web:a4af66e86b72d59c63a212"
 };
 
 // Initialize Firebase
